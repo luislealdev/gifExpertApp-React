9b050dba24a25bdaaa9c08291783970486a943a6
luislealdev <luisrrleal@gmail.com>
2022-06-09 17:44:57 -0500

    change the url to https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) =>{
-    const url = `http://api.giphy.com/v1/gifs/search?q=${category.trim()}&limit=10&api_key=JkQjY2gIMjJNXZTYq5oxaBoOr7aDZn1Y`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category.trim()}&limit=10&api_key=JkQjY2gIMjJNXZTYq5oxaBoOr7aDZn1Y`;
     const resp = await fetch(url);
     const {data} = await resp.json();
 
